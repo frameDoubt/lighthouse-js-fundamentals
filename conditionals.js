b602eb0870cf86fe762596d63e0f3442b6be1c7c
Brandon Wright <brandon.w.wright4@gmail.com>
2020-12-10 13:13:43 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,24 @@
-const temprature = -2;
+// if statements
 
-if (temprature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temprature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// const temprature = -2;
+
+// if (temprature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temprature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+// Logical Operators
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
 
-console.log("Now you're ready to go outside!");
